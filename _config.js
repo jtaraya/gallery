e3695d64dbe6e6a46c2d8cf51d8ab9db536f3f22
jtaraya <jacobtaraya>
2025-09-23 07:59:02 +0300

    MongoDB Config

--- _config.js
+++ _config.js
@@ -1,5 +1,10 @@
 var config = {}
 
+// MongoDB configuration using environment variables
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const cluster = process.env.MONGO_CLUSTER;
+
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://jtaraya:Code_Zone$23@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
