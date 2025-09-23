98e013250db2f2f4f863f9a443206e642db0aabf
jtaraya <jacobtaraya>
2025-09-24 00:10:58 +0300

    Fix uuid import and update to latest

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,48 @@
 const express = require('express');
 const router = express.Router();
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');   // ✅ updated import
 let upload = require('./upload');
-const url = require('url')
+const url = require('url');
 let Image = require('../models/images');
 
+var db = [];
 
-var db = []
-
-router.get('/', (req,res)=>{
-    
-    Image.find({}, function(err, images){
-        // console.log(images)
+router.get('/', (req, res) => {
+    Image.find({}, function(err, images) {
         if (err) console.log(err);
-        res.render('index',{images:images, msg: req.query.msg })
-    })
-})
+        res.render('index', { images: images, msg: req.query.msg });
+    });
+});
 
-router.post('/upload', (req, res)=>{
-    upload(req,res, (err)=>{
-        if (err){
+router.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
             res.redirect(`/?msg=${err}`);
-        }else{
+        } else {
             console.log(req.file);
-            // res.send("test");
-            if (req.file == undefined){
-                res.redirect('/?msg=Error: No file selcted!');
-            }else{
+            if (req.file == undefined) {
+                res.redirect('/?msg=Error: No file selected!');
+            } else {
+                // Example: using uuid to generate an ID if needed
                 // const imageObj = {
-                //     id: uuid.v4(),
+                //     id: uuidv4(),
                 //     name: req.file.filename,
                 //     path: 'images/' + req.file.filename
                 // }
                 // db.push(imageObj);
-                // console.log(db);
 
-                // create new image
                 let newImage = new Image({
                     name: req.file.filename,
                     size: req.file.size,
                     path: 'images/' + req.file.filename
-                })
+                });
 
-                // save the uploaded image to the database
-                newImage.save()
+                newImage.save();
 
-                
                 res.redirect('/?msg=File uploaded successfully');
             }
         }
-    })
-})
+    });
+});
 
 module.exports = router;
