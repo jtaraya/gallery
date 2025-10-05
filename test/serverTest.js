2cabfffc8e252d07130ff2f08f924d2fb674a4bb
jtaraya <jacobtaraya>
2025-10-05 23:00:49 +0300

    _

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,37 +1,14 @@
-<<<<<<< HEAD
-process.env.NODE_ENV = 'test';
-
-var chai = require('chai');
-const chaiHttp = require('chai-http');
-var server = require('../server'); 
-
-=======
 process.env.NODE_ENV = 'test';   
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 
 var server = require('../server');
->>>>>>> 18ece3ba0c11375ef695f05c8c5c46d1e83b4531
 var should = chai.should();
 var expect = chai.expect;
 
 chai.use(chaiHttp);
 
-<<<<<<< HEAD
-describe('Photos', function() {
-    it('should list ALL photos on / GET', function(done) {
-        this.timeout(60000);
-        chai.request(server)
-            .get('/')
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.html;
-                done();
-            });
-    });
-});
-=======
 describe('Photos', function(){
 
 
@@ -47,4 +24,3 @@ describe('Photos', function(){
         })
     });
 })
->>>>>>> 18ece3ba0c11375ef695f05c8c5c46d1e83b4531
