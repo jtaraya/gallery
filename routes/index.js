7daa2db50c5c2089959626fa17a615a61a407020
jtaraya <jacobtaraya>
2025-10-06 01:29:25 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -5,51 +5,46 @@ let upload = require('./upload');
 const url = require('url')
 let Image = require('../models/images');
 
+// GET - List all images (FIXED: converted to async/await)
+router.get('/', async (req, res) => {
+  try {
+    const images = await Image.find({});  // ← Changed from callback to await
+    res.render('index', { images: images, msg: req.query.msg });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching images');
+  }
+});
 
-
-
-router.get('/', (req,res)=>{
-    
-    Image.find({}, function(err, images){
-        // console.log(images)
-        if (err) console.log(err);
-        res.render('index',{images:images, msg: req.query.msg })
-    })
-})
-
-router.post('/upload', (req, res)=>{
-    upload(req,res, (err)=>{
-        if (err){
-            res.redirect(`/?msg=${err}`);
-        }else{
-            console.log(req.file);
-            // res.send("test");
-            if (req.file == undefined){
-                res.redirect('/?msg=Error: No file selcted!');
-            }else{
-                // const imageObj = {
-                //     id: uuid.v4(),
-                //     name: req.file.filename,
-                //     path: 'images/' + req.file.filename
-                // }
-                // db.push(imageObj);
-                // console.log(db);
-
-                // create new image
-                let newImage = new Image({
-                    name: req.file.filename,
-                    size: req.file.size,
-                    path: 'images/' + req.file.filename
-                })
-
-                // save the uploaded image to the database
-                newImage.save()
-
-                
-                res.redirect('/?msg=File uploaded successfully');
-            }
+// POST - Upload image (FIXED: converted to async/await)
+router.post('/upload', (req, res) => {
+  upload(req, res, async (err) => {  // ← Made the callback async
+    if (err) {
+      res.redirect(`/?msg=${err}`);
+    } else {
+      console.log(req.file);
+      
+      if (req.file == undefined) {
+        res.redirect('/?msg=Error: No file selected!');
+      } else {
+        try {
+          // create new image
+          let newImage = new Image({
+            name: req.file.filename,
+            size: req.file.size,
+            path: 'images/' + req.file.filename
+          });
+          
+          // save the uploaded image to the database (FIXED: added await)
+          await newImage.save();  // ← Added await here
+          res.redirect('/?msg=File uploaded successfully');
+        } catch (saveErr) {
+          console.log(saveErr);
+          res.redirect('/?msg=Error saving file to database');
         }
-    })
-})
+      }
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,54 +11,34 @@ let image = require('./routes/image');
 // Initializing the app
 const app = express();
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-// mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-//     if (err) console.log(err)
-// });
+// Get environment and database URI
 const env = process.env.NODE_ENV || 'development';
 const dbURI = config.mongoURI[env];
 
 console.log(`NODE_ENV = ${env}`);
 console.log(`Connecting to: ${dbURI}`);
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log(`Database connected successfully [${env}]`))
-.catch(err => console.error('MongoDB connection error:', err));
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
-
+// Connect to MongoDB
+mongoose.connect(dbURI)
+  .then(() => console.log(`Database connected successfully [${env}]`))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json())
-
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
 
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
 
 module.exports = app;
